21fc1e927fd5c5cdcd2d74e10b6d04745a162567
glebe <a1887500@adelaide.edu.au>
2025-02-26 23:15:39 +1030

    gif change

--- data/portfolio.js
+++ data/portfolio.js
@@ -27,7 +27,7 @@ const projects = [
   // if the array is left empty, the "Projects" section won't render
   // each element in each `description` array will be treated as a separate paragraph
   {
-    thumbnail: 'images/Winning.gif',
+    thumbnail: 'images/Winning2.gif',
     name: 'Bandle Game',
     description: [
       'Currently working on recreating the popular song guessing game, Bandle. Front-end is being built using TypeScript, React and Vite. Back-end is being built using Python FastAPI. Track separation is being performed by the Demucs Music Source Separation AI Model.',
